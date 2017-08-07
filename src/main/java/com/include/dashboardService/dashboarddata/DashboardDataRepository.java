a3001619e6189aa201d1406144261bd5d839ff9f
Aniket Verma <tf@Anikets-MacBook-Pro.local>
2017-08-07 13:47:16 +0530

    romoving code from branch

--- src/main/java/com/include/dashboardService/dashboarddata/DashboardDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.include.dashboardService.dashboarddata;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DashboardDataRepository extends MongoRepository<DashboardData, Long> {
-	
-	DashboardData findOneByCode(String code);
-
-}
